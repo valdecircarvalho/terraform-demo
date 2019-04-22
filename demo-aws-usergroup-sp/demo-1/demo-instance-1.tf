98c1f87625d53a350e7cd41f9e751f069560effa
homelaber <homelaber@gmail.com>
2019-04-22 17:39:36 -0300

    update demo-1

--- demo-aws-usergroup-sp/demo-1/demo-instance-1.tf
+++ demo-aws-usergroup-sp/demo-1/demo-instance-1.tf
@@ -1,6 +1,18 @@
+/* Você pode declarar a suas credenciais diretamente no arquio - não muito seguro
 provider "aws" { #provider AWS
   access_key = "FAKE3FGWCKVDTADPFAKE"   # coloque sua access key
   secret_key = "fAkEzr4nHhTlpkycFAKet7Ryvp+OOJ2S/nuGuu0d" #coloque sua secret key
+  region     = "us-east-1"
+} */
+
+## variaveis - serão carregadas do arquivo terraform.tfvars
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+## Você pode carregar suas credenciais de um arquivo .tfvars
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
   region     = "us-east-1"
 }
 
@@ -8,11 +20,13 @@ resource "aws_instance" "web-demo" {
   ami           = "ami-0a313d6098716f372" #ubuntu 
   instance_type = "t2.micro"
   count         = "1"
+
   # key_name          = "terraform-user"
-  
+
   tags {
-    Name = "web-demo-aws-ug-sp"
+    Name        = "web-demo-aws-ug-sp"
+    Environment = "terraform-demo"
   }
 
- #  user_data = "${file("${path.module}/src/install.sh")}"
+  #  user_data = "${file("${path.module}/src/install.sh")}"
 }
