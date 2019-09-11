d38d2d4297ae63d3da09610b53d687c2920934de
Valdecir Carvalho <valdecir.carvalho@gmail.com>
2019-09-11 13:24:46 -0300

    Update output.tf

--- demo-aws-usergroup-sp/demo-1/output.tf
+++ demo-aws-usergroup-sp/demo-1/output.tf
@@ -1,1 +1,12 @@
+output "DNS Servidor Web" {
+  value = "${aws_instance.web-demo.*.public_dns}"
+}
 
+output "IP Publico Servidor Web" {
+  value = "${aws_instance.web-demo.*.public_ip}"
+}
+
+/* output "ELB DNS" {
+  value = "${aws_elb.web.dns_name}"
+}
+ */
